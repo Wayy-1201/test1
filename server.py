7f617d57244f76a2f60abacefc9d5bba62273826
Wayy-1201 <glebsergeev919@gmail.com>
2026-03-19 06:47:16 +0400

    Update server.py

--- server.py
+++ server.py
@@ -1560,6 +1560,7 @@ document.addEventListener('DOMContentLoaded', () => {
                 }
                 updateUI();
                 loadSticker(userData.level);
+                startBubbles();
             })
             .catch(() => {
                 console.log('Сервер недоступен');
